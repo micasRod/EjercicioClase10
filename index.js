66d14e3e36749469b20faa2127a0df0d753b25a8
Mik <rodriguez.mcsol@gmail.com>
2022-05-30 20:10:20 -0300

    funcionesCompletas

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const csv = 'javascript, python, c++, rust, go,';
+let csv2=csv;
+function printValues(){
+    const valor=csv2.substring(0, csv2.indexOf(','));
+    csv2=csv2.slice(csv2.indexOf(',')+1);
+    console.log(csv2);
+    mostrarMensaje(valor);
+}
+
+function mostrarMensaje(mensaje){
+    const newValor= document.createElement('H3');
+    newValor.textContent= mensaje;
+    divMensaje.appendChild(newValor);
+    
+}
+
